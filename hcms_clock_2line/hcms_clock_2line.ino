724b894169dc0d5bae787a6c3b6e287e6a54725a
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2024-07-04 17:34:34 -0400

    2-line option
    Added version with 2 lines for date as well as time. will begin working on adjustment through buttons.

--- hcms_clock_2line/hcms_clock_2line.ino
+++ hcms_clock_2line/hcms_clock_2line.ino
@@ -5,15 +5,16 @@
 
 uint32_t time_adj = 0;
 
-LedDisplay disp = LedDisplay(11,12,13,14,15,8);
-char disp_buf[9];
+LedDisplay disp = LedDisplay(11,12,13,14,15,16);
+char disp_buf[17];
 uint32_t disp_count;
 
 void setup() {
   // put your setup code here, to run once:
   setTime(12,0,0,4,7,2024);
+  adjustTime(time_adj);
   disp.begin();
-  disp.setBrightness(5);
+  disp.setBrightness(15);
   disp.clear();
   Serial.ignoreFlowControl(true);
   Serial.begin();
@@ -22,9 +23,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   disp_count++;
-  if(disp_count == 10000){
-    sprintf(disp_buf, "%02d:%02d:%02d",hour(),minute(),second());
-    disp.clear();
+  if(disp_count == 100000){
+    sprintf(disp_buf, "%02d:%02d:%02d%02d/%02d/%02d",hour(),minute(),second(),month(),day(),(year() % 100));
     disp.home();
     disp.print(disp_buf);
     disp_count = 0;
